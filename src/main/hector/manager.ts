da45554143e1963fcf00d2ff54ea7ab06f403b55
Kadir Pekel <kadirpekel@gmail.com>
2025-12-26 04:31:53 +0100

    chore: release v0.5.0 with Hector v1.15.0 compatibility

--- src/main/hector/manager.ts
+++ src/main/hector/manager.ts
@@ -20,8 +20,8 @@ import { mergeEnvVars, formatAsEnvFile } from '../envVars/store'
 
 // Hector compatibility - should match Studio release
 const HECTOR_COMPATIBILITY = {
-    minVersion: '1.13.2',
-    recommendedVersion: '1.13.2',
+    minVersion: '1.15.0',
+    recommendedVersion: '1.15.0',
     downloadBaseUrl: 'https://github.com/verikod/hector/releases/download'
 }
 
